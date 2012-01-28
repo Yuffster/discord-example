072ba459f3f7ee7a880eae832e284025122c69f6
Michelle Steigerwalt <msteigerwalt@gmail.com>
2012-01-28 09:24:14 -0800

    More explicit world_path.

--- start_server.js
+++ start_server.js
@@ -1,6 +1,6 @@
 var config = {
 	world_name   : 'DiscordMUD',
-	world_path   : __dirname+'/world',
+	world_path   : __dirname+'/world/',
 	start_room   : 'lobby',
 	world_port   : 8000,
 	web_port     : 8080
